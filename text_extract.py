533abd9ae2f1d54dd866e804225c0c8d2913c7fe
Driti Tannk <driti.tannk@thegatewaycorp.com>
2021-06-09 13:53:39 +0530

    Modify Extraction Code
    	Remove PDF Writer Class Use.

--- text_extract.py
+++ text_extract.py
@@ -1,18 +1,17 @@
 import PyPDF2
 
-file_obj = open('paper1.pdf', 'rb')
+file_obj = open('input_files/paper1.pdf', 'rb')
 pdf_reader = PyPDF2.PdfFileReader(file_obj)  # PDF file reader object
 
-pdf_writer = PyPDF2.PdfFileWriter()  # PDF file writer object
-output_file = open('Paper1_output', 'w')
+output_file = open('Paper1_output', 'w')  # Output file
 
 total_pages = pdf_reader.getNumPages()  # Get total pages
 information = pdf_reader.getDocumentInfo()  # Get file details
-print("\n \n ------- Document Information ------ \n \n")
-print("\n \n \t", information)
-print("\n \n Total pages ==> ", total_pages)
 
-# Iterating through each page and get text
+output_file.write("Document Information: \n \n " + str(information) + "\n \n")
+output_file.write("Total Pages: \n \n " + str(total_pages) + "\n \n ")
+
+# Iterating through each page and extract text
 for page_count in range(0, total_pages):
     page_data = pdf_reader.getPage(page_count)
     output_file.write(page_data.extractText())  # Writing to text file
