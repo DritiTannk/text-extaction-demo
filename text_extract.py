6a83b1ee1a78338f7199c33ba7a219efdedefb53
Driti Tannk <driti.tannk@thegatewaycorp.com>
2021-06-17 17:53:58 +0530

    Enhancement In The Extraction Process
    	Add Method For Extracting PPT,DOC, HTML Files Content,
    	Add User Input For Input And Output Files Path.

--- text_extract.py
+++ text_extract.py
@@ -4,14 +4,14 @@ import PyPDF2
 from tika import parser
 
 
-def pdf_text_extraction(filepath, filename):
+def pdf_text_extraction(filepath, filename, output_path):
     """
     It handles text extraction process from PDF files.
     """
-    output_filename = str(filename.split('.')[0]) + '_output.txt'
+    output_filename = filename + '_output.txt'
 
     file_obj = open(filepath, 'rb')
-    output_file = open(f'output_files/{output_filename}', 'w')  # Output file
+    output_file = open(f'{output_path}/{output_filename}', 'w')  # Output file
 
     try:
         pdf_reader = PyPDF2.PdfFileReader(file_obj, strict=False)  # PDF file reader object
@@ -37,26 +37,126 @@ def pdf_text_extraction(filepath, filename):
         file_obj.close()
 
 
-def pdf_to_text_using_tikka(filepath, filename):
+def pdf_to_text_using_tikka(filepath, filename, output_path):
     """
-    This method extract text from PDF using tikka library.
+    This method extract text from PDF using tika library.
     """
     output_file_name = filename + '_tikka.txt'
     parsed_file = parser.from_file(filepath)  # Parsing PDF file
     content = parsed_file['content']  # Extracting content
 
-    with open(f'output_files/{output_file_name}', 'w') as f:
-        f.write(content.strip())
+    with open(f'{output_path}/{output_file_name}', 'w') as f:
+        res = f.write(content.strip())
+
+    return res
 
 
-if __name__ == '__main__':
-    i_path = './input_files/*'  # Input files path
+def html_to_text(filepath, filename, output_path):
+    """
+    This method extract text from HTML files using tika library.
+    """
+    output_file_name = filename + '.txt'
+    parsed_file = parser.from_file(filepath)
+    content = parsed_file['content']
 
-    for file in glob.glob(i_path):
+    with open(f'{output_path}/{output_file_name}', 'w') as file1:
+        res = file1.write(content.strip())
+
+    return res
+
+
+def ppt_to_text(filepath, filename, output_path):
+    """
+    This method extract text from power-point presentation files using tika library.
+    """
+    output_file_name = filename + '.txt'
+    parsed_file = parser.from_file(filepath)
+    content = parsed_file['content']
+
+    with open(f'{output_path}/{output_file_name}', 'w') as file1:
+        res = file1.write(content.strip())
+
+    return res
+
+
+def word_to_text(filepath, filename, o_path):
+    """
+    This method extract text from word files using tika library.
+    """
+    output_file = filename + '.txt'
+    parsed_file = parser.from_file(filepath)
+    content = parsed_file['content']
+
+    with open(f'{o_path}/{output_file}', 'w') as file1:
+        res = file1.write(content.strip())
+
+    return res
+
+
+def file_search(input_path, output_path):
+    """
+    This method gets files from given input path & checks extension.
+
+    :param input_path: input files path
+    :param output_path: output files path
+
+    :return: statistics dictionary
+    """
+    pdf_counter, ppt_counter, html_counter, word_counter = 0, 0, 0, 0
+    input_path = input_path + '/*'
+    print(input_path)
+
+    for file in glob.glob(input_path):
         fname = os.path.basename(file)  # Get file name from path
         filename, ext = os.path.splitext(fname)  # Extract filename & extension
 
         if fname.endswith('.pdf') and ext == '.pdf':
-            pdf_text_extraction(file, fname)
-            pdf_to_text_using_tikka(file, fname)
+            pdf_text_extraction(file, filename, output_path)
+            res = pdf_to_text_using_tikka(file, filename, output_path)
+
+            if res > 0:
+                pdf_counter += 1
+
+        elif fname.endswith('.html') and ext == '.html':
+            res = html_to_text(file, filename, output_path)
+
+            if res > 0:
+                html_counter += 1
+
+        elif fname.endswith('.ppt') and ext == '.ppt':
+            res = ppt_to_text(file, filename, output_path)
+
+            if res > 0:
+                ppt_counter += 1
+
+        elif fname.endswith('.doc') and ext == '.doc':
+            res = word_to_text(file, filename, output_path)
+
+            if res > 0:
+                word_counter += 1
+
+        else:
+            print(f"Invalid File Format for {filename}")
+
+    return {'pdf_counter': pdf_counter, 'html_counter': html_counter,
+            'ppt_counter': ppt_counter, 'word_counter': word_counter
+            }
+
+
+if __name__ == '__main__':
+    i_path = input('Enter The Input Path: ')  # Get user's input files directory path
+    o_path = input('Enter The Output Path: ')  # Get user's output files directory path
+
+    i_path_exist = os.path.exists(i_path)  # Check the user input path
+    o_path_exist = os.path.exists(o_path)  # Check the user output path
+
+    if i_path_exist and o_path_exist:
+        stats = file_search(i_path, o_path)
+        print(f"\n Extraction Result \n {'-' * 20}"
+              f"\n PDF Files ==> {stats.get('pdf_counter')}, "
+              f"\n HTML Files ==> {stats.get('html_counter')},"
+              f"\n PPT Files ==> {stats.get('ppt_counter')},"
+              f"\n DOC Files ==> {stats.get('word_counter')}")
+    else:
+        print("\n \n Given paths does not exists")
 
